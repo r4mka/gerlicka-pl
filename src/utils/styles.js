3552002a25cc16b7b05782af9d6aad7e3facc0ee
Adam Ramski <adam.ramski@gmail.com>
2022-09-07 18:06:49 +0200

    add eslint + prettier config

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -10,26 +10,25 @@ export const breakpoints = ['480px', '760px', '960px', '1120px'];
 export const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp})`));
 
 const colors = {
-    beige: '#F0E7DE',
-    rust: '#C06318',
-    brown: '#D1947a',
-    pink: '#E7B7AD',
-    black: '#000'
-}
+  beige: '#F0E7DE',
+  rust: '#C06318',
+  brown: '#D1947a',
+  pink: '#E7B7AD',
+  black: '#000',
+};
 
 export const theme = { colors };
 
 export const globalStyle = css`
   ${emotionNormalize}
-  
-  
+
   #___gatsby {
     min-height: 100vh;
     display: flex;
   }
-  
+
   #gatsby-focus-wrapper {
     flex: 1;
-    display: flex;    
+    display: flex;
   }
-`
+`;

d8a687140383bfaa9634c3876bcbaea0854ec88c
Adam Ramski <adam.ramski@gmail.com>
2022-09-07 18:51:35 +0200

    add manifest and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,18 @@ module.exports = {
     title: `Klaudia Gerlicka Make-Up and Brows`,
     siteUrl: `https://www.gerlicka.pl`,
   },
-  plugins: [`gatsby-plugin-emotion`],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Klaudia Gerlicka Make-Up and Brows',
+        short_name: 'Klaudia Gerlicka Make-Up',
+        start_url: '/',
+        background_color: '#F0E7DE',
+        theme_color: '#F0E7DE',
+        icon: 'src/images/icon.png',
+      },
+    },
+    `gatsby-plugin-emotion`,
+  ],
+};
